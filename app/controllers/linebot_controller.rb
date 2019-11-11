062744b1320407fb429c44e2185a19645c858c96
hiroki-12 <hiroki12malu@gmail.com>
2019-11-11 23:45:43 +0900

    取得APIを変えた。JSON形式で取得にしたので実装も大幅変更

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -11,12 +11,13 @@
 class LinebotController < ApplicationController
   require 'line/bot'  # gem 'line-bot-api'
   require 'open-uri'
-  require 'kconv'
-  require 'rexml/document'
+  require 'net/http'
+  require 'json'
 
   # callbackアクションのCSRFトークン認証を無効
   protect_from_forgery :except => [:callback]
 
+  # 参考: https://github.com/line/line-bot-sdk-ruby
   def callback
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
@@ -26,70 +27,43 @@ class LinebotController < ApplicationController
     events = client.parse_events_from(body)
     events.each { |event|
       case event
-        # メッセージが送信された場合の対応（機能①）
+        # メッセージが送信された場合の対応
       when Line::Bot::Event::Message
         case event.type
           # ユーザーからテキスト形式のメッセージが送られて来た場合
         when Line::Bot::Event::MessageType::Text
           # event.message['text']：ユーザーから送られたメッセージ
           input = event.message['text']
-          url  = "https://www.drk7.jp/weather/xml/13.xml"
-          xml  = open( url ).read.toutf8
-          doc = REXML::Document.new(xml)
-          xpath = 'weatherforecast/pref/area[4]/'
-          # 当日朝のメッセージの送信の下限値は20％としているが、明日・明後日雨が降るかどうかの下限値は30％としている
-          min_per = 30
           case input
-            # 「明日」or「あした」というワードが含まれる場合
+          when /.*(今日|きょう).*/
+            uri = URI('http://weather.livedoor.com/forecast/webservice/json/v1?city=130010')
+            weather_data = JSON.parse(Net::HTTP.get(uri))
+            day_after_tomorrow_forecast = weather_data['forecasts'][0]
+            push =
+              "#{day_after_tomorrow_forecast['dateLabel']}の天気は#{day_after_tomorrow_forecast['telop']}です。" \
+               "http://weather.livedoor.com/area/forecast/1310400"
           when /.*(明日|あした).*/
-            # info[2]：明日の天気
-            per06to12 = doc.elements[xpath + 'info[2]/rainfallchance/period[2]'].text
-            per12to18 = doc.elements[xpath + 'info[2]/rainfallchance/period[3]'].text
-            per18to24 = doc.elements[xpath + 'info[2]/rainfallchance/period[4]'].text
-            if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
-              push =
-                "明日の天気だよね。\n明日は雨が降りそうだよ(>_<)\n今のところ降水確率はこんな感じだよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
-            else
-              push =
-                "明日の天気？\n明日は雨が降らない予定だよ(^^)\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
-            end
+            uri = URI('http://weather.livedoor.com/forecast/webservice/json/v1?city=130010')
+            weather_data = JSON.parse(Net::HTTP.get(uri))
+            tomorrow_forecast = weather_data['forecasts'][1]
+            push =
+               "#{tomorrow_forecast['dateLabel']}の天気は#{tomorrow_forecast['telop']}です。" \
+               "最高気温は#{tomorrow_forecast['temperature']['max']['celsius']}度、" \
+               "最低気温は#{tomorrow_forecast['temperature']['min']['celsius']}度です。" \
+               "http://weather.livedoor.com/area/forecast/1310400"
           when /.*(明後日|あさって).*/
-            per06to12 = doc.elements[xpath + 'info[3]/rainfallchance/period[2]l'].text
-            per12to18 = doc.elements[xpath + 'info[3]/rainfallchance/period[3]l'].text
-            per18to24 = doc.elements[xpath + 'info[3]/rainfallchance/period[4]l'].text
-            if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
-              push =
-                "明後日の天気だよね。\n何かあるのかな？\n明後日は雨が降りそう…\n当日の朝に雨が降りそうだったら教えるからね！"
-            else
-              push =
-                "明後日の天気？\n気が早いねー！何かあるのかな。\n明後日は雨は降らない予定だよ(^^)\nまた当日の朝の最新の天気予報で雨が降りそうだったら教えるからね！"
-            end
-          when /.*(かわいい|可愛い|カワイイ|きれい|綺麗|キレイ|素敵|ステキ|すてき|面白い|おもしろい|ありがと|すごい|スゴイ|スゴい|好き|頑張|がんば|ガンバ).*/
+            uri = URI('http://weather.livedoor.com/forecast/webservice/json/v1?city=130010')
+            weather_data = JSON.parse(Net::HTTP.get(uri))
+            day_after_tomorrow_forecast = weather_data['forecasts'][2]
             push =
-              "ありがとう！！！\n優しい言葉をかけてくれるあなたはとても素敵です(^^)"
-          when /.*(こんにちは|こんばんは|初めまして|はじめまして|おはよう).*/
+              "#{day_after_tomorrow_forecast['dateLabel']}の天気は#{day_after_tomorrow_forecast['telop']}です。" \
+               "http://weather.livedoor.com/area/forecast/1310400"
+          when /.*(アメリカ|あめりか).*/
             push =
-              "こんにちは。\n声をかけてくれてありがとう\n今日があなたにとっていい日になりますように(^^)"
+              "隠しコマンドを良く見つけたね！アメリカの思い出が恋しいよ"
           else
-            per06to12 = doc.elements[xpath + 'info/rainfallchance/period[2]l'].text
-            per12to18 = doc.elements[xpath + 'info/rainfallchance/period[3]l'].text
-            per18to24 = doc.elements[xpath + 'info/rainfallchance/period[4]l'].text
-            if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
-              word =
-                ["雨だけど元気出していこうね！",
-                 "雨に負けずファイト！！",
-                 "雨だけどああたの明るさでみんなを元気にしてあげて(^^)"].sample
-              push =
-                "今日の天気？\n今日は雨が降りそうだから傘があった方が安心だよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\n#{word}"
-            else
-              word =
-                ["天気もいいから一駅歩いてみるのはどう？(^^)",
-                 "今日会う人のいいところを見つけて是非その人に教えてあげて(^^)",
-                 "素晴らしい一日になりますように(^^)",
-                 "雨が降っちゃったらごめんね(><)"].sample
-              push =
-                "今日の天気？\n今日は雨は降らなさそうだよ。\n#{word}"
-            end
+            push =
+              "明日の天気が知りたい場合は#{'明日orあした'}。　\n明後日の天気が知りたい場合は#{'明後日orあさって'}とテキストしてね！　 "
           end
           # テキスト以外（画像等）のメッセージが送られた場合
         else
@@ -117,10 +91,10 @@ class LinebotController < ApplicationController
 
   private
 
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
+    def client
+      @client ||= Line::Bot::Client.new { |config|
+        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      }
+    end
 end
